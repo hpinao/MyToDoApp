844ebcb9ba4df82eecc0722b83648acd4f73b0ea
hpinao <hugo.pinao@hotmail.com>
2024-05-14 13:37:49 +0000

    fourth version

--- app.py
+++ app.py
@@ -20,3 +20,15 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/add", methods=["POST"])
+def add_todo():
+    todo = request.form["todo"]
+    todo_list.append(todo)
+    save_todo_list()
+    return redirect(url_for("index"))
+
+def save_todo_list():
+    with open(todo_file, "w") as file:
+        for todo in todo_list:
+            file.write(todo + "\n")
